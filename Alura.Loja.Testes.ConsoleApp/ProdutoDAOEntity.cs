45e9d21d9267e22296f7796dd22b33fe38b68833
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-02-05 20:31:27 -0300

    Add project files.

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoDAOEntity.cs
@@ -0,0 +1,47 @@
+﻿using Alura.Loja.Testes.ConsoleApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+  class ProdutoDAOEntity : IProdutoDAO, IDisposable
+  {
+    private LojaContext _context;
+
+    public ProdutoDAOEntity()
+    {
+      _context = new LojaContext();
+    }
+
+    public void Adicionar(Produto p)
+    {
+      _context.Produtos.Add(p);
+      _context.SaveChanges();
+    }
+
+    public void Atualizar(Produto p)
+    {
+      _context.Produtos.Update(p);
+      _context.SaveChanges();
+    }
+
+    public void Remover(Produto p)
+    {
+      _context.Produtos.Remove(p);
+      _context.SaveChanges();
+    }
+
+    public IList<Produto> Produtos()
+    {
+      return _context.Produtos.ToList();
+    }
+
+    public void Dispose()
+    {
+      _context.Dispose();
+    }
+  }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //GravarUsandoAdoNet();
+      GravarUsandoEntity();
+    }
+
+    private static void AtualizarProduto()
+    {
+
+      using (var repo = new ProdutoDAOEntity())
+      {
+        Produto primeiro = repo.Produtos().First();
+        primeiro.Nome = "Cassino Royale - Editado";
+        repo.Atualizar(primeiro);
+      }
+    }
+    private static void GravarUsandoEntity()
+    {
+      Produto p = new Produto();
+      p.Nome = "Cassino Royale";
+      p.Categoria = "Filmes";
+      p.Preco = 19.89;
+
+      using (var repo = new ProdutoDAOEntity())
+      {
+        repo.Adicionar(p);
+      }
+    }
+
+    private static void ExcluirProduto()
+    {
+      using (var repo = new ProdutoDAOEntity())
+      {
+        IList<Produto> produtos = repo.Produtos().ToList();
+        foreach (var item in produtos)
+        {
+          repo.Remover(item);
+        }
+      }
+    }
+
+    private static void RecuperarProdutos()
+    {
+      using (var repo = new ProdutoDAOEntity())
+      {
+        IList<Produto> produtos = repo.Produtos().ToList();
+        Console.WriteLine($"Foram econtrados {produtos.Count} produtos(s)");
+
+        foreach (var item in produtos)
+        {
+          Console.WriteLine(item.Nome);
+        }
+      }
+    }
+  }
+}
